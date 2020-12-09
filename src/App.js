be5a205005b822d6df7bf96740c51fb10815eb34
Otavio F <38397566+otavio-f@users.noreply.github.com>
2020-12-09 20:03:54 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,119 +1,108 @@
 import React, { Component } from 'react';
-import './App.css';
+//import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      playerScore: 0,
-      questions: [
+      score: 0,
+      questionary: [
         {
-          question: 'Javascript é uma linguagem _________ ?',
-          possibleAnswers: ['Compilada', 'Interpretada'],
-          rightAnswer: 'Interpretada',
+          question: "What animal barks?",
+          answers: ["Dog", "Cat"],
+          rightAnswer: "Dog",
           playerChoice: null
         },
         {
-          question: "Se na função: function soma (a, b) { return a + b }, você passar os argumentos (2, 3, 4, 5), qual o resultado obtido ",
-          possibleAnswers: ['2', '5', '9', '14'],
-          rightAnswer: '5',
+          question: "What animal is more closely related to a tiger?",
+          answers: ["Dog", "Cat"],
+          rightAnswer: "Cat",
           playerChoice: null
         },
         {
-          question: 'O operador condicional (ternário) é o único operador JavaScript que possui três operandos. A sintaxe correta é',
-          possibleAnswers: ['condicao ? certo : errado', 'If (condicao){certo}', 'condicao: certo ? errado', '123'],
-          rightAnswer: 'condicao ? certo : errado',
+          question: "What animal is more closely related to a wolf?",
+          answers: ["Dog", "Cat"],
+          rightAnswer: "Dog",
           playerChoice: null
         },
         {
-          question: 'Que tipo de valor será retornado na expressão: var winstonsAge = 50;',
-          possibleAnswers: ['objeto', 'numero', 'string'],
-          rightAnswer: 'numero',
+          question: "What animal is best known for playing fetch?",
+          answers: ["Dog", "Cat"],
+          rightAnswer: "Dog",
           playerChoice: null
         }
       ]
     }
-
     this.answerQuestion = this.answerQuestion.bind(this);
   }
 
-  displayQuestion(index) {
-    if(this.state.playerScore < index) { return; }
+  answerQuestion(index, choice) {
+    const answeredQuestion = this.state.questionary[index];
+    answeredQuestion.playerChoice = choice;
+    const allQuestions = this.state.questionary;
+    allQuestions[index] = answeredQuestion;
+    this.setState({
+      questions: allQuestions
+    }, () => {
+      this.updatescore();
+    });
+  }
 
-    const question = this.state.questions[index];
+  updatescore() {
+    const score = this.state.questionary.filter(q => q.rightAnswer === q.playerChoice).length;
+    this.setState({ score });
+    console.log("New player score:", score);
+  }
+
+  displayResult(index) {
+    const question = this.state.questionary[index];
+    if (!question.playerChoice) { return; }
+    if (question.playerChoice === question.rightAnswer) {
+        return (
+          <p className="result-correct">
+            Your answer is correct!
+          </p>
+        );
+    } else {
+      return (
+        <p className="result-incorrect">
+          Your answer is incorrect!
+        </p>
+      );
+    }
+  }
+
+  displayQuestion(index) {
+    if (this.state.score < index) { return; }
+    const question = this.state.questionary[index];
     return (
-      <div className="question-display" key={`q-${index}`}>
-        <h3 className="question">
+      <div className="question-display">
+        <p className="question">
           {question.question}
-        </h3>
+        </p>
         <br />
-        
-        { question.possibleAnswers.map((answer, answerIndex) => (
-          <button key={`q-${index}-a-${answerIndex}`} className="question-choice" onClick={() => this.answerQuestion(index, answer)}>
-            {answer}
-          </button>
-        ))
-        }
-        
-        
+        <button className="question-choice" onClick={() => this.answerQuestion(index, question.answers[0])}>
+          {question.answers[0]}
+        </button>
+        <button className="question-choice" onClick={() => this.answerQuestion(index, question.answers[1])}>
+          {question.answers[1]}
+        </button>
         <br />
         {this.displayResult(index)}
       </div>
     );
   }
-
-  answerQuestion(index, choice) {
-    const answeredQuestion = this.state.questions[index];
-    answeredQuestion.playerChoice = choice;
-    const allQuestions = this.state.questions;
-    allQuestions[index] = answeredQuestion;
-
-    this.setState({
-      questions: allQuestions
-    }, () => {
-      this.updatePlayerScore();
-    })
-  }
-
-  updatePlayerScore() {
-    const playerScore = this.state.questions.filter(q => q.rightAnswer === q.playerChoice).length;
-    this.setState({playerScore});
-    console.log(`New player score: ${playerScore}`);
-  }
-
-  displayResult(index) {
-    const question = this.state.questions[index];
-
-    if(!question.playerChoice) { return; }
-
-    if(question.playerChoice === question.rightAnswer) {
-      return (
-        <p className="result-correct">
-          Resposta certa!
-        </p>
-      );
-    } else {
-      return (
-        <p className="result-incorrect">
-          Resposta errada!
-        </p>
-      );
-    }
-  }
-
-  renderQuestions() {
-    return this.state.questions.map((question, index) => this.displayQuestion(index));
-  }
-
+  
   render() {
     return (
       <div className="App">
-        <h1 className="title" >Quiz Show!</h1>
+        <h1>Quiz Show!</h1>
         <hr/>
-        <div className="answers">
-          {this.renderQuestions()}
-        </div>
+        {this.displayQuestion(0)}
+        {this.displayQuestion(1)}
+        {this.displayQuestion(2)}
+        {this.displayQuestion(3)}
       </div>
     );
   }
